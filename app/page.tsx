02defd0b8e7afdfe4e02c2639c8079642deaa1cf
RegRoLLL <156439463+RegRoLLL@users.noreply.github.com>
2026-02-19 00:45:58 +0900

    色変更

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,9 @@ export default function Home() {
 
   return (
     <div className="font-mono bg-indigo-100 max-w-3xl min-h-screen mx-auto flex flex-col items-center p-10 gap-3">
-      <a className="text-3xl font-bold">アイコンリングセッター!!（仮設）</a>
+      <a className="text-3xl font-bold text-gray-800">
+        アイコンリングセッター!!（仮設）
+      </a>
       <div className="h-1 w-full rounded-sm bg-white" />
       <ImageCanvas iconImg={iconImg} ringImg={ringImg} />
       <div className="flex flex-col gap-3 items-">
